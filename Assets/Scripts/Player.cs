ead815d6f278d0236a4759ab2cef6ce7c61ffb6e
Doctorbingbing <36774670+Doctorbingbing@users.noreply.github.com>
2018-02-23 18:29:13 -0800

    Wrote some pick functionality into player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,59 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MovingObject {
+
+	public int Health = 100;
+	public int Speed = 50;
+	public int Damage = 5
 
 	// Use this for initialization
-	void Start () {
+	protected override void Start () {
 		
+		base.Start();
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	private void OnTriggerGiveBuff (Collider2D  other)
+	{
+		if (other.tag == "Speed")
+		{
+			Speed = 75 //Currently permanent, would like to implement a timed duration
+			return
+		}
+
+		if (other.tag == "DamageUp")
+		{
+			Damage = 8
+			return
+		}
+
+		if (other.tag == "heal")
+		{
+			Health += 15
+			return
+		}
+
+		elseif
+		{
+			return
+		}
+	}
+
+	public void LoseHP (int loss)
+	{
+		Health -= loss;
+		CheckIfGameover();
+	}
+	private void CheckIfGameover()
+	{
+		if (Health <= 0)
+			// INSERT CODE FOR ENDING GAME
 	}
 }
