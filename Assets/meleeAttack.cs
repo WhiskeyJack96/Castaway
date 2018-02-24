ea1742b40d5762c8c1d6f1f14bd5e50574b69543
superchewy <nnallan@uci.edu>
2018-02-23 21:44:27 -0800

    spritefixup
    if you changed used the sprites before this, this might create errors

--- Assets/meleeAttack.cs
+++ Assets/meleeAttack.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class meleeAttack : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
