f00133aa46af77b75fafe749914d236b8d796c83
Doctorbingbing <36774670+Doctorbingbing@users.noreply.github.com>
2018-02-24 20:04:10 -0800

    Implemented Camera Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+public GameObject Player;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Player.transform.position;
+
+
+		//Get player.transform.position, set camera position to this
+		
+	}
+}
