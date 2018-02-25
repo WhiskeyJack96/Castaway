ae36b8be42e49e5c0df311e90bd113733bfdac09
WhiskeyJack96 <Jacob.mikesell96@gmail.com>
2018-02-24 22:11:51 -0800

    Added Resource Spawning To the map

---
+++ Assets/Scripts/SpawnTerrain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTerrain : MonoBehaviour {
+	public GameObject Terrain;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void spawn()
+    {
+    	if(Terrain != null)
+    	{
+    		Instantiate(Terrain, transform.position, new Quaternion(0,0,0,0));
+    	}
+        
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
