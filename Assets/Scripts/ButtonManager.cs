476b685e27adc35f6d79117004fb0523e955391e
StatFarm <36776084+StatFarm@users.noreply.github.com>
2018-02-25 15:29:25 -0800

    Main Menu Scene
    Did Main Menu

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonManager : MonoBehaviour {
+	 
+	void Start(){
+		int ds = PlayerPrefs.GetInt ("DifficultySetting");
+	}
+	public void NewGameBtn (string NewGame)
+	{ 
+		SceneManager.LoadScene ("JakesScene");
+
+	}
+	public void ExitGameBtn ()
+	{
+		Application.Quit ();
+	}
+	public void DifficultyInput (int ds)   //small/easy = 0, med/intermediate = 1, large/hard = 2
+	{
+		PlayerPrefs.SetInt ("DifficultySetting", ds);
+
+	}
+}
