1690f918c9527578d87211310e005741398750a8
WhiskeyJack96 <Jacob.mikesell96@gmail.com>
2018-02-24 18:59:19 -0800

    Created Ground Prefabs and Added a map generation script.
    Generates map using perlin noise and converts it to a 50 by 50 grid of tiles

--- Assets/Scripts/NoiseMap.cs
+++ Assets/Scripts/NoiseMap.cs
@@ -8,19 +8,31 @@ public class NoiseMap : MonoBehaviour {
     public float o3 = .25f;
     public float o4 = .1f;
     public float o5 = 1f;
-    public int power = 3;
+    public float power = 3;
     private float seed1;
+    private List<GameObject> map;
+
+    public GameObject grass;
+    public GameObject sand;
+    public GameObject mountain;
+    public GameObject icy;
+    public GameObject scarysand;
+
     private int seed2;
-    private float listy= 100;
-    private float listx = 100;
-    List<float> locations;
+    private float listy= 50;
+    private float listx = 50;
+    List<int> locations;
 
 
 	// Use this for initialization
 	void Start () {
-        locations = new List<float>();
+        map = new List<GameObject>();
+        locations = new List<int>();
         seed1 =Random.Range(1,10000);
         seed2 =Random.Range(1,10000);
+
+        //print(grass.GetComponent<Collider2D>().bounds);
+
 
 		for(int i = 0; i <listx; i++)
         {
@@ -34,7 +46,7 @@ public class NoiseMap : MonoBehaviour {
                     );
 
                 heightpoint /= 2f;
-                //noisepoint =Mathf.Pow(noisepoint, power);
+                heightpoint =Mathf.Pow(heightpoint, power);
                 //locations.Add(heightpoint);
 
                 float waterpoint = (o5*Mathf.PerlinNoise(seed2 + (i/listx - .5f),seed2 + (j/listy - .5f))+
@@ -45,16 +57,100 @@ public class NoiseMap : MonoBehaviour {
                     );
 
                 waterpoint /= 2f;
+                waterpoint =Mathf.Pow(waterpoint, power);
+                int biome = GetBiome(heightpoint,waterpoint);
+                locations.Add(biome);
 
-                
-                //noisepoint =Mathf.Pow(noisepoint, power);
+
                 //locations.Add(noisepoint);
             }
         }
-        locations.Sort();
-        print(locations[100*100-1]);
+        convertToMap();
     }
 	
+    private void convertToMap()
+    {
+        int num = 0;
+        Vector3 nextcoord = new Vector3(0,0,0);
+
+        for(int i = 0; i <listx; i++)
+        {
+            for(int j = 0; j <listy; j++)
+            {
+                print(nextcoord);
+                switch(locations[i + (j*(int)listy)])
+                {
+                    case 0:
+                    {
+                        map.Add(Instantiate (grass, nextcoord, new Quaternion(0,0,0,0)));
+                        num+=1;
+                        break;
+                    }
+                    case 1:
+                    {
+                        map.Add(Instantiate (mountain, nextcoord, new Quaternion(0,0,0,0)));
+                        num+=1;
+                    break;
+                    }
+                    case 2:
+                    {
+                        map.Add(Instantiate (sand, nextcoord, new Quaternion(0,0,0,0)));
+                        num+=1;
+                    break;
+                    }
+                    case 3:
+                    {
+                        map.Add(Instantiate (icy, nextcoord, new Quaternion(0,0,0,0)));
+                        num+=1;
+                    break;
+                    }
+                    case 4:
+                    {
+                        map.Add(Instantiate (scarysand, nextcoord, new Quaternion(0,0,0,0)));
+                        num+=1;
+                    break;
+                    }
+                }
+                nextcoord= new Vector3(nextcoord.x + .25f, nextcoord.y, nextcoord.z);
+                if ((num % listy) == 0)
+                {
+                    nextcoord= new Vector3(0 , nextcoord.y+.25f, nextcoord.z);
+                }
+            }
+        }
+    }
+
+
+    private int GetBiome(float height, float water)
+    {
+        if(height<.5f && water<.5f)
+        {
+            return 0; //grass
+        }
+        else if(height>.5f && water <.4f)
+        {
+            return 1; //mountain
+        }
+        else if(water>.5f && height <.4f)
+        {
+            return 2; //sand
+        }
+        else if(height>.5f && water >.4f && water <.7f)
+        {
+            return 3; //Icy
+        }
+        else if(water>.5f && height>.4f && height <.7f)
+        {
+            return 4; //Scary Sand
+        }
+        else
+        {
+            return 0;//grassy
+    }
+
+
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
