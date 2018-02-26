364dab57fc10caa125ac1233c7567b21d4740fc1
Doctorbingbing <36774670+Doctorbingbing@users.noreply.github.com>
2018-02-25 19:16:55 -0800

    idek

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Timer : MonoBehaviour {
 
 public int scorenum;
+private int totalscore;
+private bool death;
 
 
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating(timer(),0,0.1);
+		InvokeRepeating("timer", 0f, 1.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		death = GetComponent<hopefullyhealthbar>.isDead;
+		if (GetComponent<hopefullyhealthbar>.isDead)
+		{
+			totalscore = scorenum;
+			print(totalscore);
+			CancelInvoke();
+		}
 	}
 	public void timer() 
 	{
