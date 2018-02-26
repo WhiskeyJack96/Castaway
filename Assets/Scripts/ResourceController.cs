579bac37719b5080cf6fbc0a6b3e33caf260cd50
WhiskeyJack96 <Jacob.mikesell96@gmail.com>
2018-02-25 20:40:32 -0800

    Added rudimentary wall spawning

--- Assets/Scripts/ResourceController.cs
+++ Assets/Scripts/ResourceController.cs
@@ -8,6 +8,9 @@ public class ResourceController : MonoBehaviour {
 	private string Wood;
 	public int TotalWood;
 	private int amount;
+	public GameObject wall;
+	public bool canBuild = true;
+	public float timer = .1f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +20,7 @@ public class ResourceController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		place();	
 	}
 
 	public void addresource(string resource, int amount)
@@ -36,4 +39,19 @@ public class ResourceController : MonoBehaviour {
 		}
 	}
 
+	public void place()
+	{
+		if(Input.GetKeyDown(KeyCode.F) && TotalWood>0 && canBuild)
+		{
+			Instantiate(wall, Input.mousePosition, new Quaternion(0,0,0,0));
+			StartCoroutine(cooldown(timer));
+
+		}
+	}
+
+	IEnumerator cooldown(float timer){
+        canBuild = false;
+        yield return new WaitForSeconds(timer);
+        canBuild = true;
+    }
 }
