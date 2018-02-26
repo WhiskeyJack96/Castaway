00363bfe8f85644e46010e53c5b70ee3eac84607
Doctorbingbing <36774670+Doctorbingbing@users.noreply.github.com>
2018-02-25 19:03:38 -0800

    Added Timer Script
    Can change score with score function where score is simply the number of seconds since alive.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+public int scorenum;
+
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating(timer(),0,0.1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void timer() 
+	{
+		++scorenum;
+		
+	}
+	public void score(int change)
+	{
+		scorenum += change;
+	}
+
+}
