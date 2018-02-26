a6107cab9709ebb40e9d49b2ec2256becc33eb14
StatFarm <36776084+StatFarm@users.noreply.github.com>
2018-02-25 23:08:52 -0800

    Death Scene

---
+++ Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class DeathScreen : MonoBehaviour {
+	public Text ScoreText;
+	public GameObject GC;
+	//public Score = GetComponent.GameController<>().;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ScoreText.text = "Final Score is " + GC.GetComponent<ScoreController>().total;
+	}
+
+	public void RestartBtn()
+	{
+		SceneManager.LoadScene ("TestMain");
+	}
+	public void MainMenuBtn()
+	{
+		SceneManager.LoadScene ("Main Menu");
+	}
+}
